53c9484c0a2236107c66a721ce5c89222b4e8291
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-03-02 22:03:46 +0200

    comit4

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,32 +1,35 @@
 // import { useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { selectTheme, setUserTheme } from 'store/Header/themeSlice';
-import CSS from './Theme.module.css';
-export const Theme = () => {
-  //   const dispatch = useDispatch();
-  //   const theme = useSelector(selectTheme);
-  //   const [showThemesMenu, setShowThemesMenu] = useState(false);
+// import CSS from './Theme.module.css';
+// export const Theme = () => {
+//   const dispatch = useDispatch();
+//   const theme = useSelector(selectTheme);
+//   const [showThemesMenu, setShowThemesMenu] = useState(false);
 
-  //   const handleThemeChange = selectedTheme => {
-  //     dispatch(setUserTheme(selectedTheme));
-  //   };
-  return (
-    <>
-      {/* <div className="menu">
-        {showThemesMenu ? (
-          <div className="themes-menu" onClick={() => setShowThemesMenu(false)}>
-            <select
-              value={theme}
-              onChange={e => handleThemeChange(e.target.value)}
-            >
-              <option value="light">light</option>
-              <option value="dark">dark</option>
-              <option value="purple">purple</option>
-            </select>
-          </div>
-        ) : (
-            </div> */}
-      <button className={CSS.theme_btn}>Themes</button>
-    </>
-  );
-};
+//   //   const handleThemeChange = selectedTheme => {
+//   //     dispatch(setUserTheme(selectedTheme));
+//   //   };
+//   return (
+//     <div ref={themeRef} onClick={handlePopup}>
+//       <Text>Theme</Text>
+//       <Icon isOpen={isShownPopup}>
+//         <use href={sprite + '#icon-chevron-down'} />
+//       </Icon>
+
+//       {isShownPopup && (
+//         <PopupBlock>
+//           {theme.map(({ name }) => (
+//             <PopupItem
+//               onClick={() => handleTheme(name)}
+//               key={name}
+//               className={activeUserTheme === name ? 'active' : ''}
+//             >
+//               {name[0].toUpperCase() + name.slice(1)}
+//             </PopupItem>
+//           ))}
+//         </PopupBlock>
+//       )}
+//     </div>
+//   );
+// };
